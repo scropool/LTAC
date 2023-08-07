9df8d44ed6c28e4843afad34ba1a479e888ffed5
buadambabaisi <81594192+buadambabaisi@users.noreply.github.com>
2023-08-07 18:42:04 +0300

    New version: Code Cleanup

---
+++ Windows 10 LTSC & LTSB Activator v1.2.bat
@@ -0,0 +1,15 @@
+@echo off
+title Windows 10 LTSC-LTSB Activator v1.2
+color 9
+echo Windows 10 LTSC-LTSB Activation Tool v1.2 by buadambabaisi
+ping localhost -n 3 >nul
+cls
+ping localhost -n 3 >nul
+echo Please Wait..
+slmgr.vbs /ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
+slmgr /skms kms8.msguides.com
+slmgr.vbs /ato
+cls
+ping localhost -n 2 >nul
+echo Windows 10 LTSC-LTSB Activated!
+ping localhost -n 2 >nul
